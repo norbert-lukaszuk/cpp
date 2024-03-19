ad281976f1a45e100b9882371c76b46179358e56
norbert-lukaszuk <norbert.lukaszuk@google.com>
2024-03-19 16:55:14 +0100

    .vscode/c_cpp_properties.json: add path to compiler

--- hello_world/hello.cpp
+++ hello_world/hello.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+
 using namespace std;
+int count_months(int years);
 int main()
 {
+    int age;
     cout << "Hello world!";
+    cout << endl;
+    cout << "How old are you?";
+    cin >> age;
+    int months = count_months(age);
+    cout << "You are " << months << " months old";
     cout << endl;
 
     return 0;
+}
+
+int count_months(int years)
+{
+    int months;
+    months = years * 12;
+
+    return months;
 }
